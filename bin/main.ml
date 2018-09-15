184a66da32de78787a1d6bf6b75213148a56df32
Josef Erben <josef@erben.io>
2018-09-15 16:56:01 +0200

    Read lines from file and print all lines

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,24 @@
-open Lib
+open Printf
+
+let help = "Invalid argument, use -s <keyword> for searching."
+
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
+let print_lines lines =
+  List.fold_left (fun acc s -> acc ^ s ^ "\n") "" lines
+
+let parseArgs args = match args with
+    [| |] -> "empty" |
+    [| _; truth |] -> "Adding truth: " ^ truth |
+    [| _; "-s"; _ |] -> print_lines(read_lines ".memb") |
+    _ -> help
 
 let () =
-  let result = Math.add 2 3 in
-  print_endline (string_of_int result);
-  let result = Math.sub 3 1 in
-  print_endline (string_of_int result)
+  printf "%s\n" (parseArgs Sys.argv)
